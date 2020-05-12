8b31c8e20396abde4ddf456d76b66a1e6147f76f
Zack <zackbubb19@gmail.com>
2020-05-12 17:45:25 -0400

    added new endpoint for month entries

--- pfApi/pfApiApp/urls.py
+++ pfApi/pfApiApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('labels', views.getLabels, name='labels'),
+    path('month/<int:monthId>/year/<int:year>/entries', views.getMonthEntries, name='monthEntries'),
     path('month/<int:monthId>/year/<int:year>/overview', views.getMonthOverview, name='monthOverview')
 ]
--- pfApi/pfApiApp/views.py
+++ pfApi/pfApiApp/views.py
@@ -11,6 +11,11 @@ def getLabels(request):
     return HttpResponse(data, content_type='application/json')
 
 # TODO account for year
+def getMonthEntries(request, monthId, year):
+    monthEntrySet = MonthEntry.objects.filter(entryDate__month = (monthId + 1)).order_by('entryDate')
+    data = serialize('json', monthEntrySet)
+    return HttpResponse(data, content_type='application/json')
+
 def getMonthOverview(request, monthId, year):
     monthEntrySet = MonthEntry.objects.filter(entryDate__month = (monthId + 1))
     data = json.dumps(getMonthOverviewObject(monthEntrySet))
