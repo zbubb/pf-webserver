bb1d63da0736dcb4f6ef7dd280e94be7a907863f
Zack <zackbubb19@gmail.com>
2020-05-14 18:09:57 -0400

    added create and edit endpoints

--- pfApi/pfApiApp/urls.py
+++ pfApi/pfApiApp/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('labels', views.getLabels, name='labels'),
+    path('label', views.createLabel, name='createLabel'),
+    path('labels', views.getLabels, name='getLabels'),
     path('month/<int:monthId>/year/<int:year>/entries', views.getMonthEntries, name='monthEntries'),
     path('month/<int:monthId>/year/<int:year>/overview', views.getMonthOverview, name='monthOverview'),
-    path('month/entry/<int:entryId>', views.getMonthEntry, name='monthEntry')
+    path('month/entry/create', views.createMonthEntry, name='createMonthEntry'),
+    path('month/entry/<int:entryId>', views.getMonthEntry, name='monthEntry'),
+    path('month/entry/<int:entryId>/edit', views.editMonthEntry, name='editMonthEntry'),
 ]
--- pfApi/pfApiApp/views.py
+++ pfApi/pfApiApp/views.py
@@ -1,13 +1,51 @@
-from django.core.serializers import serialize
 from django.http import HttpResponse
+from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 from .models import *
 from .utils import  *
 
-# Create your views here.
+#TODO error handling
+#TODO check for correct http request type
+#TODO better csrf stuff
+
 def getLabels(request):
     data = serialize('json', Label.objects.all())
+    return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def createLabel(request):
+    createLabelDto = json.loads(request.body)
+    newLabel = Label(label_text = createLabelDto['label_text'])
+    newLabel.save()
+    data = serialize('json', [newLabel])[1:-1]
+    return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def createMonthEntry(request):
+    createEntryDto = json.loads(request.body)
+    newEntry = MonthEntry(
+            entryDate = createEntryDto['entryDate'],
+            amount = createEntryDto['amount'],
+            isPositive = createEntryDto['isPositive'],
+            label = Label.objects.get(pk = createEntryDto['label'])
+            )
+    newEntry.save()
+    entryId = newEntry.id
+    data = serialize('json', [MonthEntry.objects.get(pk = entryId)])[1:-1]
+    return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def editMonthEntry(request, entryId):
+    updateEntryDto = json.loads(request.body)
+    monthEntry = MonthEntry.objects.get(pk = entryId)
+    monthEntry.entryDate = updateEntryDto['entryDate']
+    monthEntry.amount = updateEntryDto['amount']
+    monthEntry.isPositive = updateEntryDto['isPositive']
+    monthEntry.label = Label.objects.get(pk = updateEntryDto['label'])
+    monthEntry.save()
+    data = serialize('json', [MonthEntry.objects.get(pk = entryId)])[1:-1]
     return HttpResponse(data, content_type='application/json')
 
 def getMonthEntry(request, entryId):
